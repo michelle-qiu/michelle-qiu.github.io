ab0e19393052e82eee05ee2bfe512607249e5ace
qiumic000 <michelle.qiu@duke.edu>
2022-06-22 22:48:57 -0500

    wip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import profile from ".//Images/Portrait.jpg";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="profile">
+      <h1>This is my personal website work-in-progress!</h1>
     </div>
   );
 }
-
+//<img src={profile} className = "profile" alt="Profile picture"></img>
 export default App;
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App/>,rootElement);
